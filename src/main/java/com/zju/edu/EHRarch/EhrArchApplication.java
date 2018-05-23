6ec4f68df48fdcb6674cd7cbb6b02a76ff14db53
lmy86263 <lmy_86263@163.com>
2018-05-23 10:28:00 +0800

    init EHR-arch

---
+++ src/main/java/com/zju/edu/EHRarch/EhrArchApplication.java
@@ -0,0 +1,12 @@
+package com.zju.edu.EHRarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhrArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhrArchApplication.class, args);
+	}
+}
---
+++ src/main/java/com/zju/edu/EHRarch/components/ArchetypeHandler.java
@@ -0,0 +1,12 @@
+package com.zju.edu.EHRarch.components;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lmy86263
+ * @date 2018/05/20
+ */
+@Component("archetypeHandler")
+public class ArchetypeHandler {
+
+}
